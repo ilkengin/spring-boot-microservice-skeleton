a5939ff0ae88e2fc6cee25da4a6beadf9bc63186
Ilkan Engin <ilkan.engin@siemens.com>
2020-06-25 22:49:08 +0300

    Initialized config-server

---
+++ config-server/src/test/java/com/ilkengin/core/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkengin.core.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
