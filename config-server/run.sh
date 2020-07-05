77804507e1c15e48f8ae674a1b96e96e198545f5
Ilkan Engin <ilkan.engin@gmail.com>
2020-07-05 16:39:43 +0300

    Dockerized config-server

---
+++ config-server/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+echo "********************************************************"
+echo "Starting config-server "
+echo "********************************************************"
+java   $MEM_ARGS -Dspring.profiles.active=$PROFILE -jar app.jar
