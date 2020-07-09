7500a4651935e963a03df5ec16452ffce477cbd7
Ilkan Engin <ilkan.engin@gmail.com>
2020-07-09 23:56:17 +0300

    Extended JwtTokenProvider to validate the given token

--- gateway/src/main/java/com/ilkengin/core/gateway/provider/JwtTokenProvider.java
+++ gateway/src/main/java/com/ilkengin/core/gateway/provider/JwtTokenProvider.java
@@ -2,10 +2,14 @@ package com.ilkengin.core.gateway.provider;
 
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.ilkengin.core.gateway.dto.WebUser;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -13,24 +17,56 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Service
 public class JwtTokenProvider {
-
-	private String secretKey;
+	@Value("${security.jwt.token.expiration}")
 	private long validityInMilliseconds;
 
+	private String secretKey;
+	
 	@Autowired
-	public JwtTokenProvider(@Value("${security.jwt.token.secret-key}") String secretKey,
-			@Value("${security.jwt.token.expiration}") long milliseconds) {
+	public JwtTokenProvider(@Value("${security.jwt.token.secret-key}") String secretKey) {
 		this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
-		this.validityInMilliseconds = milliseconds;
 	}
-
+	
+	// Build the Token
 	public String createToken(String username) {
-		Claims claims = Jwts.claims().setSubject(username);
-
-		// Build the Token
-		Date now = new Date();
-		return Jwts.builder().setClaims(claims).setIssuedAt(now)
-				.setExpiration(new Date(now.getTime() + validityInMilliseconds))
+		return Jwts.builder()
+				.setClaims(new HashMap<>())
+				.setSubject(username)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + validityInMilliseconds))
 				.signWith(SignatureAlgorithm.HS256, secretKey).compact();
+	}
+	
+	//validate token
+	public boolean isTokenValid(String token, WebUser user) {
+		final String username = getUsernameFromToken(token);
+		return (username.equals(user.getUsername()) && !isTokenExpired(token));
+	}
+	
+
+	public String getUsernameFromToken(String token) {
+		return getClaimFromToken(token, Claims::getSubject);
+	}
+		
+	//check if the token has expired
+	private boolean isTokenExpired(String token) {
+		final Date expiration = getExpirationDateFromToken(token);
+		return expiration.before(new Date());
+	}
+	
+	//retrieve expiration date from jwt token
+	private Date getExpirationDateFromToken(String token) {
+		return getClaimFromToken(token, Claims::getExpiration);
+	}
+	
+
+	private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = getAllClaimsFromToken(token);
+		return claimsResolver.apply(claims);
+	}
+	
+    //for retrieveing any information from token we will need the secret key
+	private Claims getAllClaimsFromToken(String token) {
+		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
 	}
 }
