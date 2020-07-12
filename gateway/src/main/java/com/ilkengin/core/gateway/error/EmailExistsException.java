ce97243873c6b3e3fe9967a95b6c66c4d9112cf6
Ilkan Engin <ilkan.engin@gmail.com>
2020-07-12 17:49:48 +0300

    Added exceptions for handling user creation

---
+++ gateway/src/main/java/com/ilkengin/core/gateway/error/EmailExistsException.java
@@ -0,0 +1,6 @@
+package com.ilkengin.core.gateway.error;
+
+public class EmailExistsException extends Exception {
+	private static final long serialVersionUID = -6174939263396759941L;
+
+}
---
+++ gateway/src/main/java/com/ilkengin/core/gateway/error/UsernameExistsException.java
@@ -0,0 +1,8 @@
+package com.ilkengin.core.gateway.error;
+
+public class UsernameExistsException extends Exception {
+	private static final long serialVersionUID = 7656642913675071734L;
+
+	
+	
+}
